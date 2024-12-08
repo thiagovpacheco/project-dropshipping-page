453f9af8f99e9ce92663cb208d8f7e1dd7b48817
Thiago Pacheco <thiagovpacheco@gmail.com>
2024-12-08 11:31:31 -0300

    fix: Simplifica configuração do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,10 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  base: './',
 });
